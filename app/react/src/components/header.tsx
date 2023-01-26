5b93a0975bac0b1e023ac61bc977c096573463c3
sillypoise <somewhatjose@gmail.com>
2023-01-26 12:39:10 -0500

    @app/react: interactive works!

--- app/react/src/components/header.tsx
+++ app/react/src/components/header.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 function Header() {
+    const [count, setCount] = useState(0);
     return (
         <article>
             <h1>Component</h1>
             <p>FTW</p>
+            <span>{count} </span>
+            <button onClick={() => setCount((c) => c + 1)}>-</button>
+            <button onClick={() => setCount((c) => c - 1)}>+</button>
         </article>
     );
 }
