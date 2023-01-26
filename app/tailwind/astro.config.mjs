5e8fcd45136120d4d202708d1784e262a327aaf2
sillypoise <somewhatjose@gmail.com>
2023-01-26 11:04:22 -0500

    @app/tailwindcss: reverting to postcss plugin

--- app/tailwind/astro.config.mjs
+++ app/tailwind/astro.config.mjs
@@ -3,5 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+    ],
 });
